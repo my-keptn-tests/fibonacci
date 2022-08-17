366c8f04f32dd4b2c8c459268a7e61b52c03a6dc
keptn <keptn@keptn.sh>
2022-08-17 15:03:00 +0000

    Updated resource

--- fibo/k6/calculate.js
+++ fibo/k6/calculate.js
@@ -3,6 +3,6 @@ import { sleep } from 'k6';
 
 export default () => {
   const BASE_URL = __ENV.BASE_URL || 'fibo.fibonacci-staging.svc.cluster.local:80';
-  http.get(`${BASE_URL}/calculate?num=40`);
+  http.get(`http://${BASE_URL}/calculate?num=40`);
   sleep(1);
 };
