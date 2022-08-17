3781be187c0d83801d036a7ea442afeff25d9ca0
keptn <keptn@keptn.sh>
2022-08-17 14:50:50 +0000

    Updated resource

--- fibo/k6/calculate.js
+++ fibo/k6/calculate.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default () => {
-  const BASE_URL = __ENV.BASE_URL || 'http://localhost:30000';
+  const BASE_URL = __ENV.BASE_URL || 'fibo.fibo-staging.sv.cluster.local:80';
   http.get(`${BASE_URL}/calculate?num=40`);
   sleep(1);
 };
