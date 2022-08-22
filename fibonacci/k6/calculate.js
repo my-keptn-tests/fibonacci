21764c04024cb469da393123f91e938d17e550c0
keptn <keptn@keptn.sh>
2022-08-22 15:01:39 +0000

    Updated resource

--- fibonacci/k6/calculate.js
+++ fibonacci/k6/calculate.js
@@ -3,6 +3,13 @@ import { sleep } from 'k6';
 
 export default () => {
   const BASE_URL = __ENV.BASE_URL || 'fibo.fibonacci-staging.svc.cluster.local:80';
-  http.get(`http://${BASE_URL}/calculate?num=40`);
+  const res = http.get(`http://${BASE_URL}/calculate?num=40`);
+  if (
+    !check(res, {
+      'status code MUST be 200': (res) => res.status == 200,
+    })
+  ) {
+    fail('status code was *not* 200');
+  }
   sleep(1);
 };
