d92377bb755c40b6d1f953f3adac04b5fee34c17
keptn <keptn@keptn.sh>
2022-08-23 17:59:18 +0000

    Updated resource

--- fibonacci/k6/calculate.js
+++ fibonacci/k6/calculate.js
@@ -4,7 +4,7 @@ import { check, fail, sleep } from 'k6';
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.05'], // http errors should be less than 1%
+    http_req_failed: ['rate<0.1'], // http errors should be less than 10%
     // http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
   },
 };
